c84dc1bd37e410cee3d8d1eeed08ca0a5f13564b
sreejithnair190 <sreejith190nair@gmail.com>
2023-07-18 23:13:04 +0530

    products

--- src/components/products/products.jsx
+++ src/components/products/products.jsx
@@ -1,20 +1,28 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 
-const Products = ({ product }) => {
-  const [products, setProducts] = useState([]);
 
-  if (product == undefined) {
-    product = "plants";
-  }
+const ProductList = ({ productItem }) => {
+  const { name, description, price } = productItem;
+  return (
+    <div className="product-card">
+      <h2>{name}</h2>
+      <p>{description}</p>
+      <p>Price: ${price}</p>
+    </div>
+  );
+}
+
+const Products = ({ productName }) => {
+  const [products, setProducts] = useState([]);
 
   const fetchProducts = async () => {
     try {
       const response = await axios.get(
-        `${process.env.REACT_APP_API_URL}/${product}`
+        `${process.env.REACT_APP_API_URL}/${productName}`
       );
-      setProducts(response.data.data);
-      console.log(products);
+      setProducts(response.data.data.data);
+      console.log(response.data.data.data);
     } catch (error) {
       console.log(error);
     }
@@ -26,19 +34,15 @@ const Products = ({ product }) => {
     };
   }, []);
 
-  if (products.length > 0) {
-    products[0].map((product) => {
-      console.log(product);
-    });
-  }
-
   return (
-    <div className="plants">
-      <h3>Plants</h3>
-      <div className="plants-container">
-        {products.map((product) => (
-          JSON.stringify(product)
-        ))}
+    <div className="products">
+      <h3>{productName.charAt(0).toUpperCase() + productName.slice(1)}</h3>
+      <div className="products-container">
+        {products.length > 0
+          ? products.map((product) => (
+              <ProductList key={product._id} productItem={product} />
+            ))
+          : console.log("No Products Found")}
       </div>
     </div>
   );
