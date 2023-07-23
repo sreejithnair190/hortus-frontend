e195251f07137762a724bf8fdd7bd23ba1eb80f9
sreejithnair190 <sreejith190nair@gmail.com>
2023-07-23 19:31:52 +0530

    navbar style changes

--- src/components/home/navbar/navbar.jsx
+++ src/components/home/navbar/navbar.jsx
@@ -1,7 +1,7 @@
-import { React, Fragment } from 'react'
-import { Link, Outlet, useLocation } from 'react-router-dom'
-import './navbar.css'
-
+import { React, Fragment } from "react";
+import { Link, Outlet, useLocation } from "react-router-dom";
+// import './navbar.css'
+import "./navbar.scss";
 
 const Navbar = () => {
   const location = useLocation();
@@ -12,22 +12,58 @@ const Navbar = () => {
   const isHome = currentPath === "/";
 
   return (
-    <Fragment>
-      <div className='navbar-container'>
-        <Link className='hortus-logo' to='/'>
-          <img className='logo' src="img/logo.png" alt="Hortus Logo" />
+    // <Fragment>
+    <header class="header" id="header">
+      <nav className="nav container">
+        <Link className="nav__logo" to="/">
+          <i class="ri-leaf-line nav__logo-icon"></i> Hortus
         </Link>
-        <div className="nav-links">
-          <Link className="nav-link" to='/'>Contact us</Link>
-          <Link className="nav-link" to='/'>Wishlist</Link>
-          {isHome && <Link className="nav-link" to='/auth/sign-in'>Sign In</Link> }
-          {isSignIn && <Link className="nav-link" to='/auth/sign-up'>Sign Up</Link> }
-          {isSignUp && <Link className="nav-link" to='/auth/sign-in'>Sign In</Link> }
+        <div className="nav__menu" id="nav-menu">
+          <ul className="nav__list">
+            <li className="nav__item">
+              <Link className="nav__link active-link" to="/">
+                About Us
+              </Link>
+            </li>
+            <li className="nav__item">
+              <Link className="nav__link active-link" to="/">
+                FAQs
+              </Link>
+            </li>
+            <li className="nav__item">
+              <Link className="nav__link active-link" to="/">
+                Contact Us
+              </Link>
+            </li>
+            <li className="nav__item">
+              <Link className="nav__link active-link" to="/">
+                Login
+              </Link>
+            </li>
+          </ul>
         </div>
-      </div>
+        {/* <div className="nav__list">
+            <Link className="nav__item" to="/">
+              About Us
+            </Link>
+            <Link className="nav__item" to="/">
+              FAQs
+            </Link>
+            <Link className="nav__item" to="/">
+              Contact us
+            </Link>
+            <Link className="nav__item" to="/">
+              Login
+            </Link> */}
+        {/* {isHome && <Link className="nav-link" to='/auth/sign-in'>Sign In</Link> }
+          {isSignIn && <Link className="nav-link" to='/auth/sign-up'>Sign Up</Link> }
+          {isSignUp && <Link className="nav-link" to='/auth/sign-in'>Sign In</Link> } */}
+        {/* </div> */}
+      </nav>
       <Outlet />
-    </Fragment>
-  )
-}
+    </header>
+    // </Fragment>
+  );
+};
 
 export default Navbar;
